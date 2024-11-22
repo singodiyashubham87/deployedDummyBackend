431101f04579899728eaa2210be5c5dfc83d76e6
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-11-22 12:34:58 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-undef */
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3001;
+
+const FILE_NAME = 'dummy.png';
+
+app.use(express.json());
+app.use(cors());
+
+const routes = {
+    download: '/download',
+    ping: '/ping',
+};
+
+app.get(routes.ping, (req, res) => {
+  res.status(200).send({ status: 'ok' });
+});
+app.get(routes.download, async (req, res) => {
+    try {
+        res.sendFile(FILE_NAME, { root: "./" });
+    } catch (error) {
+        console.error(`Error sending file: ${error.message}`);
+        res.status(500).send('Error sending file');
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
