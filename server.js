1b69897b5a5877ab40dfa6b354efccd255f2aa39
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-11-22 12:40:36 +0530

    update port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const FILE_NAME = 'dummy.png';
 
